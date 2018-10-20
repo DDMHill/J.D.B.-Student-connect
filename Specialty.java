02fd24f112b3a440a52ccee9aa7890057377170a
Ben3940 <35115507+Ben3940@users.noreply.github.com>
2018-10-20 03:24:05 -0400

    Add files via upload

---
+++ Specialty.java
@@ -0,0 +1,9 @@
+package search;
+
+public enum Specialty {
+    WEB_DEVELOPER, // index 0
+    APP_DEVELOPER, // index 1
+    FRONT_END, // index 2
+    BACK_END, // index 3
+    MOBILE_DEVELOPER; //index 4
+}
