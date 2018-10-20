3645921afd324304bafe347e25f89c8675d09c4e
D.Hill <hillmarsoc@yahoo.com>
2018-10-20 03:39:20 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -5,17 +5,31 @@ import java.util.Scanner;
  */
 public class Database {
     Scanner input = new Scanner(System.in);
-    
-    public void addField(){
+    Student student = new Student();
+    public void addRecord(String name,String major,boolean availablity,int ID,int age){
+        //STANDARD METHOD WITH ALL FIELDS
+        student.setName(name);
+        student.setMajor(major);
+        student.setAvailablity(availablity);
+        student.setID(ID);
+        student.setAge(age);               
+    }
+    public void addRecord(){
         int field = 0;
-        //CLEAR SCREEN
-        System.out.println("Select field to add:\n1. Name\n2. Major\n3. Availablity\n4. ID\n5. Age\n6. Ability");
-        field = input.nextInt();
+        field = selectionMenu();
+        if(field == 1)
+            
         
     }
-    public void addRecord(String name,String major,boolean availablity,int ID,int age){
-        
-        
+    private int selectionMenu(){
+        int field = 0;
+        System.out.println("Select field to add:\n1. Name\n2. Major\n3. Availablity\n4. ID\n5. Age\n6. Ability");
+        field = input.nextInt();
+        while(field > 6 || field < 1){
+           System.out.println("Select field to add:\n1. Name\n2. Major\n3. Availablity\n4. ID\n5. Age\n6. Ability");
+           field = input.nextInt();
+        }            
+        return field;
     }
     
     
